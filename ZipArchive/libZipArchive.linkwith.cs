2c6eb5704d631025ab9459257b4f00904ef607ef
Matt O'Connor <moconnor@obdsoftware.net>
2015-02-04 16:27:19 -0500

    Updated native and managed projects to support 64-bit arm and simulator builds.

--- ZipArchive/libZipArchive.linkwith.cs
+++ ZipArchive/libZipArchive.linkwith.cs
@@ -1,4 +1,4 @@
 using System;
 using ObjCRuntime;
 
-[assembly: LinkWith ("libZipArchive.a", LinkTarget.Simulator | LinkTarget.ArmV7, SmartLink = true, ForceLoad = true)]
+[assembly: LinkWith ("libZipArchive.a", LinkTarget.Simulator | LinkTarget.Simulator64 | LinkTarget.ArmV7 | LinkTarget.Arm64, SmartLink = true, ForceLoad = true)]
